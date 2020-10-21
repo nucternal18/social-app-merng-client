db697bf8cdf7eb9378dc41d01009c37031116fa4
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-10-21 17:01:29 +0100

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@ap
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://lit-meadow-75873.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
